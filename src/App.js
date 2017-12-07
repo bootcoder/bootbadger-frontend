67044b727763edc743f08b44481a2a5380bda979
BootCoder <bootcoder@gmail.com>
2017-12-07 13:03:14 -0800

    reg form with handlers

--- src/App.js
+++ src/App.js
@@ -11,16 +11,22 @@ class App extends Component {
     this.state = {
       loginEmail: '',
       loginPassword: '',
-      loginName: '',
+      loginName: 'Jenna',
       jwtToken: window.localStorage.getItem('authToken'),
       showLoginForm: false,
-      showCreateForm: true
+      showRegistrationForm: true
     }
 
     this.updateLoginEmail = this.updateLoginEmail.bind(this)
     this.updateLoginPassword = this.updateLoginPassword.bind(this)
+    this.updateLoginName = this.updateLoginName.bind(this)
     this.handleUserLoginRequest = this.handleUserLoginRequest.bind(this)
     this.renderAuth = this.renderAuth.bind(this)
+    this.handleUserRegistrationRequest = this.handleUserRegistrationRequest.bind(this)
+  }
+
+  updateLoginName (name) {
+    this.setState({loginName: name})
   }
 
   updateLoginEmail (email) {
@@ -42,7 +48,28 @@ class App extends Component {
       console.log(data)
       appTarget.setState({
         loginPassword: '',
-        jwtToken: data.token
+        jwtToken: data.token,
+        showLoginForm: false
+      })
+      window.localStorage.setItem('authToken', data.token)
+    }).catch(data => {
+      window.alert(data)
+    })
+  }
+
+  handleUserRegistrationRequest () {
+    const appTarget = this
+    const payload = `?boot[email]=${this.state.loginEmail}&boot[password]=${this.state.loginPassword}&boot[name]=${this.state.loginName}`
+    window.fetch('https://bootbadger.herokuapp.com/users' + payload, {
+      method: 'POST'
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      appTarget.setState({
+        loginPassword: '',
+        jwtToken: data.token,
+        showRegistrationForm: false
       })
       window.localStorage.setItem('authToken', data.token)
     }).catch(data => {
@@ -62,7 +89,7 @@ class App extends Component {
         />
       )
     }
-    if (this.state.showCreateForm === true) {
+    if (this.state.showRegistrationForm === true) {
       return (
         <RegisterUserForm
           loginName={this.state.loginName}
---
+++ src/RegisterUserForm.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react'
+
+class RegisterUserForm extends Component {
+  constructor () {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleNameChange = this.handleNameChange.bind(this)
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.handleUserLoginRequest()
+  }
+
+  handleEmailChange (event) {
+    this.props.updateLoginEmail(event.target.value)
+  }
+
+  handlePasswordChange (event) {
+    this.props.updateLoginPassword(event.target.value)
+  }
+
+  handleNameChange (event) {
+    this.props.updateLoginName(event.target.value)
+  }
+
+  render () {
+    return (
+      <div className='RegisterUserForm'>
+        <form onSubmit={this.handleSubmit}>
+          Email: <input
+            onChange={this.handleEmailChange}
+            type='text'
+            value={this.props.loginEmail}
+          />
+          Password: <input
+            onChange={this.handlePasswordChange}
+            type='password'
+            value={this.props.loginPassword}
+          />
+          <select name='loginName' value={this.props.loginName} onChange={this.handleNameChange}>
+            <option value='Jenna'>Jenna</option>
+            <option value='Olivia'>Olivia</option>
+            <option value='Shannon'>Shannon</option>
+            <option value='Kenn'>Kenn</option>
+            <option value='Loraine'>Loraine</option>
+            <option value='Ally'>Ally</option>
+            <option value='James'>James</option>
+            <option value='Hunter'>Hunter</option>
+            <option value='Caroline'>Caroline</option>
+            <option value='Matthew'>Matthew</option>
+            <option value='May'>May</option>
+            <option value='Sar'>Sar</option>
+            <option value='Kam'>Kam</option>
+            <option value='Anthony'>Anthony</option>
+            <option value='Shambhavi'>Shambhavi</option>
+            <option value='Quynh'>Quynh</option>
+            <option value='Josh'>Josh</option>
+            <option value='Dillon'>Dillon</option>
+            <option value='Joe'>Joe</option>
+            <option value='Roger'>Roger</option>
+            <option value='Rebekah'>Rebekah</option>
+            <option value='Ken'>Ken</option>
+            <option value='Mark'>Mark</option>
+          </select>
+          <input type='submit' value='Login' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default RegisterUserForm
